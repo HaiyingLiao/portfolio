d022d09f2a3fea516b1653343761fea359ab67d4
Hannah-Liao <hanna.liao@outlook.com>
2023-09-15 20:07:50 +0100

    refactor: use next/font

--- frontend_next/app/(root)/layout.tsx
+++ frontend_next/app/(root)/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { Toaster } from "@/components/ui/toaster";
+import { Poppins } from "next/font/google";
 
 import "../globals.css";
 import { NavBar, Footer, CallToAction } from "@/components/index";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Haiying Liao's portfolio",
@@ -13,7 +19,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <NavBar />
 
         <main className="fadeIn">
