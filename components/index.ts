afabd2bb74bb37702363effd9e5aa14dd8b44925
Hannah-Liao <hanna.liao@outlook.com>
2023-12-01 17:36:09 +0000

    feat: implement email copy func

--- components/index.ts
+++ components/index.ts
@@ -1,33 +1,35 @@
-import Info from "./caseStudy/Info";
-import TechStack from "./caseStudy/TechStack";
-import CallToAction from "./CallToAction";
-import ContactForm from "./caseStudy/ContactForm";
-import Experience from "./Experience";
-import Footer from "./Footer";
-import NavBar from "./NavBar";
-import ProjectCard from "./ProjectCard";
-import Service from "./Service";
-import Techs from "./Techs";
-import Testimonial from "./Testimonial";
-import Process from "./caseStudy/Process";
-import Gains from "./caseStudy/Gains";
-import RelevantCaseStudies from "./caseStudy/RelevantCaseStudies";
-import MyInfo from "./caseStudy/MyInfo";
+import Info from './caseStudy/Info';
+import TechStack from './caseStudy/TechStack';
+import CallToAction from './CallToAction';
+import ContactForm from './caseStudy/ContactForm';
+import Experience from './Experience';
+import Footer from './Footer';
+import NavBar from './NavBar';
+import ProjectCard from './ProjectCard';
+import Service from './Service';
+import Techs from './Techs';
+import Testimonial from './Testimonial';
+import Process from './caseStudy/Process';
+import Gains from './caseStudy/Gains';
+import RelevantCaseStudies from './caseStudy/RelevantCaseStudies';
+import MyInfo from './caseStudy/MyInfo';
+import EmailCopy from './EmailCopy';
 
 export {
-  Info,
-  TechStack,
-  CallToAction,
-  ContactForm,
-  Experience,
-  Footer,
-  NavBar,
-  ProjectCard,
-  Service,
-  Techs,
-  Testimonial,
-  Process,
-  Gains,
-  RelevantCaseStudies,
-  MyInfo,
+	Info,
+	TechStack,
+	CallToAction,
+	ContactForm,
+	Experience,
+	Footer,
+	NavBar,
+	ProjectCard,
+	Service,
+	Techs,
+	Testimonial,
+	Process,
+	Gains,
+	RelevantCaseStudies,
+	MyInfo,
+	EmailCopy,
 };
